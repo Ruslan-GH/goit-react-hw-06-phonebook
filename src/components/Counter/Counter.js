73a682298fe8b0c833243b6d98041d0b23bfb021
Ruslan-GH <kr.safetyok@gmail.com>
2023-03-06 15:26:08 +0200

    1

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Controls from './Controls';
 import Value from './Value';
 import './Counter.css';
 
-export default function Counter () {
-    return (
-      <div className="Counter">
-        <Value value={value} />
+function Counter() {
+  return (
+    <div className="Counter">
+      <Value value={value} />
 
-        <Controls
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-        />
-      </div>
-    );
+      <Controls
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+      />
+    </div>
+  );
 }
-
 
 export default connect()(Counter);
